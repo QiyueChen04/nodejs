920d2581d1cddabfa033daa62f2470e331e6a4cb
Qiyue Chen <jackchen20048@gmail.com>
2024-12-26 23:34:34 -0500

    curl unit test

--- server/server.js
+++ server/server.js
@@ -2,4 +2,25 @@ const express = require("express");
 const app = express();
 const PORT = 8383;
 
+const data = {
+  name: "data",
+};
+
+// HTML repsonse
+app.get("/", (req, res) => {
+  console.log('Sending HTML res')
+  res.send(`
+	<body style="">
+	  <h1>Data: </h1>
+	  <p>${JSON.stringify(data)}</p>
+	</body>
+  `);
+});
+
+// DATA response
+app.get("/api/data", (req, res) => {
+  console.log("get method for returning data");
+  res.send(data);
+});
+
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
