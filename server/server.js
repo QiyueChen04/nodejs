6ee8e90636658b50437bdf83a413d84b0faed93e
Qiyue Chen <jackchen20048@gmail.com>
2024-12-28 11:07:47 -0500

    post req

--- server/server.js
+++ server/server.js
@@ -3,12 +3,14 @@ const app = express();
 const PORT = 8383;
 
 const data = {
-  name: "data",
+  users: ['name1']
 };
+
+app.use(express.json());
 
 // HTML repsonse
 app.get("/", (req, res) => {
-  console.log('Sending HTML res')
+  console.log("Sending HTML res");
   res.send(`
 	<body style="">
 	  <h1>Data: </h1>
@@ -21,6 +23,14 @@ app.get("/", (req, res) => {
 app.get("/api/data", (req, res) => {
   console.log("get method for returning data");
   res.send(data);
+});
+
+// POST
+app.post("/api/data", (req, res) => {
+  console.log("post req");
+  const newEntry = req.body;
+  data.users.push(newEntry.name);
+  res.sendStatus(201);
 });
 
 app.listen(PORT, () => console.log(`Server has started on ${PORT}`));
